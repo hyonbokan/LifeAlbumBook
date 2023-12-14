79c44496134e2549981a48ce794e0a8e2cc9e9e2
hyonbokan <mkkanhb@gmail.com>
2023-12-14 17:13:58 +0900

    testing firebase

---
+++ LifeAlbumBook/LifeAlbumBook/Managers/AuthManager.swift
@@ -0,0 +1,31 @@
+//
+//  AuthManager.swift
+//  LifeAlbumBook
+//
+//  Created by dnlab on 12/14/23.
+//
+
+import Foundation
+import FirebaseAuth
+
+class AuthManager {
+    static let shared = AuthManager()
+    
+    private let auth = Auth.auth()
+    
+    public func signIn(
+        email: String,
+        password: String,
+        completion: @escaping (Bool) -> Void
+    ) {
+        auth.signIn(withEmail: email, password: password) { result, error in
+            guard result != nil, error == nil else {
+                completion(false)
+                print("Could not sign in")
+                return
+            }
+            completion(true)
+        }
+    }
+    
+}
--- LifeAlbumBook/LifeAlbumBook/ViewController.swift
+++ LifeAlbumBook/LifeAlbumBook/ViewController.swift
@@ -8,10 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let signInButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Sign In", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 18, weight: .bold)
+        button.backgroundColor = .systemBlue
+        return button
+    }()
+    
+    private let registerButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Register", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 18, weight: .bold)
+        button.backgroundColor = .systemBlue
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        view.addSubview(signInButton)
+        configureButtons() 
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let buttonSize: CGFloat = 100
+        signInButton.frame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
+    }
+    
+    private func configureButtons() {
+        signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+        view.addSubview(signInButton)
+    }
+    
+    @objc private func didTapSignIn() {
+        AuthManager.shared.signIn(email: "testAcc", password: "123123123") { result in
+            if result {
+                print("\nsign in success!\n")
+            } else {
+                print("\nCould not sign in\n")
+            }
+        }
     }
 
 
